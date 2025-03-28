389fe8a7a898301b1f237bbc6b2bdea57a4f7332
Stephen Nnamani <s.nnamani@outlook.com>
2025-03-28 22:07:23 +0100

    Update DataAPI.js

--- DataAPI.js
+++ DataAPI.js
@@ -1,11 +1,25 @@
 //let url = "https://reqres.in/api/users"
-let url = "http://freeapi.miniprojectideas.com/api/Annadata/GetAllCities"
-var data = dataCollection(url)
+// let url = "http://freeapi.miniprojectideas.com/api/Annadata/GetAllCities"
+// var data = dataCollection(url)
 
-async function dataCollection(url) {
-    var data = await fetch(url).then(res => res.json()).catch(error => console.log(error))
-    return data
-}
+// async function dataCollection(url) {
+//     var data = await fetch(url).then(res => res.json()).catch(error => console.log(error))
+//     return data
+// }
+
+// Array of JSON objects
+const city =[
+  { name: "John", age: 30, isMarried: false },
+  { name: "Jane", age: 25, isMarried: true },
+  { name: "Alex", age: 35, isMarried: false },
+  { name: "Mary", age: 28, isMarried: true },
+  { name: "Paul", age: 40, isMarried: true },
+  { name: "Anna", age: 22, isMarried: false },
+  { name: "Steve", age: 50, isMarried: true },
+  { name: "Laura", age: 27, isMarried: false },
+  { name: "Mike", age: 33, isMarried: true },
+  { name: "Emma", age: 29, isMarried: false },
+];
 
 const container = document.getElementById("List of countries")
 
